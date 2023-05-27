7b2591ebacdf73d1c61fb5c1e3bba993dce8ae81
Epiphany <zxjOvO@gmail.com>
2023-05-27 10:47:25 +0800

    redisTemplate.opsForValue() operate String data type

--- src/test/java/com/zxj/SpringDataApplicationTests.java
+++ src/test/java/com/zxj/SpringDataApplicationTests.java
@@ -2,12 +2,29 @@ package com.zxj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
+/**
+ * redisTemplate.opsForValue()   操作字符串
+ * redisTemplate.opsForHash()    操作hash
+ * redisTemplate.opsForList()    操作list
+ * redisTemplate.opsForSet()     操作set
+ * redisTemplate.opsForZSet()    操作有序set
+ */
 @SpringBootTest
 class SpringDataApplicationTests {
 
+
+    private RedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+        // 写入一条String类型数据
+        redisTemplate.opsForValue().set("name", "zxj");
+
+        // 获取String数据
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println("name = " + name);
     }
 
 }
